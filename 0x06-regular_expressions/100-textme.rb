0544a14959d64320ad6c91a9400dc2eab0e878c8
bdftyousra@gmail.com <bdftyousra@gmail.com>
2023-07-04 22:14:26 +0000

    task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from|to|flags):(\+?\w+|[-?[0-1]:?]+)/).join(',')
+
+log_file = ARGV[0]
+
+File.open(log_file, 'r') do |file|
+  file.each_line do |line|
+    match = line.match(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/)
+    if match
+      sender = match[1]
+      receiver = match[2]
+      flags = match[3]
+      puts "#{sender},#{receiver},#{flags}"
+    end
+  end
+end
+
