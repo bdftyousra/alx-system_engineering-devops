5dd5a192e78a4fbccc4331a69774a0d46a53ebfc
bdftyousra@gmail.com <bdftyousra@gmail.com>
2023-09-26 10:44:17 -0700

    crection

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""
-Task 0 - Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+'''
+Python script that returns information using REST API
+'''
+import requests
+from sys import argv
 
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-
-    rq = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                      format(argv[1]))
-    rqname = rq.json().get('name')
-
-    rq = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                      format(argv[1]))
-    rqdata = rq.json()
-    done = total = 0
-    for task in rqdata:
-        total += 1
-        if task.get('completed'):
-            done += 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(rqname, done, total))
-    for task in rqdata:
-        if task.get('completed'):
-            print('\t {}'.format(task.get('title')))
+if __name__ == "__main__":
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
